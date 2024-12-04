fd3b2f58efc0d17738a46dbd81faefbfcb8a663d
Lenny Atomz <lenny.atomz@gmail.com>
2024-12-03 22:09:09 -0500

    Add simple validations to Content record

--- src/main/java/dev/lennyadams/contentcalendar/model/Content.java
+++ src/main/java/dev/lennyadams/contentcalendar/model/Content.java
@@ -1,14 +1,38 @@
 package dev.lennyadams.contentcalendar.model;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
+/**
+ * A record of content, where some of the content is validated.
+ * @param id Identifier of the content
+ * @param title Title of the content
+ * @param description Content description
+ * @param status Status of the content
+ * @param contentType Type of the content
+ * @param dateCreated Date of content creation
+ * @param dateUpdated Date of content update
+ * @param url Uniform Resource Locator
+ */
 public record Content(
+
         Integer id,
+
+        @NotBlank
         String title,
+
+        @NotBlank
         String description,
+
         Status status,
+
         Type contentType,
+
+        @NotBlank
         LocalDateTime dateCreated,
+
         LocalDateTime dateUpdated,
+
         String url) {
 }
